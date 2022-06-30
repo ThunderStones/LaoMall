03063a279466f5c9881dced1969977bec2dab949
Woog <879068849@qq.com>
2022-06-30 17:30:55 +0800

    init project

---
+++ src/main/java/org/csu/laomall/LaoMallApplication.java
@@ -0,0 +1,13 @@
+package org.csu.laomall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LaoMallApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LaoMallApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/csu/laomall/LaoMallApplicationTests.java
@@ -0,0 +1,13 @@
+package org.csu.laomall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LaoMallApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
